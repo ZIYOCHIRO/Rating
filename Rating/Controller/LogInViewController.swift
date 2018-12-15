e1833f325d4e6c8db10b89ec90238bd62b62692d
ZIYOCHIRO <ziyochiro@gmail.com>
2018-12-15 22:18:25 +0800

    ADD LogInViewController and SignUpViewController

---
+++ Rating/Controller/LogInViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LogInViewController.swift
+//  Rating
+//
+//  Created by 10.12 on 2018/12/15.
+//  Copyright © 2018 Rui. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LogInViewController: UIViewController {
+    
+}
---
+++ Rating/Controller/SignUpViewContoller.swift
@@ -0,0 +1,14 @@
+//
+//  SignUpViewContoller.swift
+//  Rating
+//
+//  Created by 10.12 on 2018/12/15.
+//  Copyright © 2018 Rui. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+}
