a804e126b127877c61da6e46f422f6a462d868c2
ZIYOCHIRO <ziyochiro@gmail.com>
2018-12-16 13:56:32 +0800

    Connected to the Firebase, got the first user -- That's me! Hahahaha :)

--- Rating/Controller/SignUpViewContoller.swift
+++ Rating/Controller/SignUpViewContoller.swift
@@ -8,8 +8,11 @@
 
 import Foundation
 import UIKit
+import Firebase
 
-class SignUpViewController: UIViewController {
+class SignUpViewController: UIViewController, UITextFieldDelegate {
+    
+    
     @IBOutlet weak var goBackButton: UIButtonX!
     @IBOutlet weak var userNameField: UITextField!
     @IBOutlet weak var emailField: UITextField!
@@ -21,14 +24,50 @@ class SignUpViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addVerticalGradientLayer(topColor: primaryColor, bottomColor: secondaryColor)
+        
+        continueButton = RoundedWhiteButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+        continueButton.setTitleColor(secondaryColor, for: .normal)
+        continueButton.setTitle("Continue", for: .normal)
+        continueButton.titleLabel?.font = UIFont.systemFont(ofSize: 18.0, weight: UIFont.Weight.bold)
+        continueButton.center = CGPoint(x: view.center.x, y: view.frame.height - continueButton.frame.height - 24)
+        continueButton.highlightedColor = UIColor(white: 1.0, alpha: 1.0)
+        continueButton.defaultColor = UIColor.white
+        continueButton.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside)
+        
+        view.addSubview(continueButton)
+        setContinueButton(enabled: false)
+        
+        activityView = UIActivityIndicatorView(style: .gray)
+        activityView.color = secondaryColor
+        activityView.frame = CGRect(x: 0, y: 0, width: 50.0, height: 50.0)
+        activityView.center = continueButton.center
+        view.addSubview(activityView)
+        
+        userNameField.delegate = self
+        emailField.delegate = self
+        passwordField.delegate = self
+        
+        userNameField.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
+        emailField.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
+        passwordField.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        
+        super.viewWillAppear(animated)
+        userNameField.becomeFirstResponder()
+        NotificationCenter.default.addObserver(self, selector:#selector(keyboardWillAppear), name: UIResponder.keyboardWillShowNotification, object: nil)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        userNameField.resignFirstResponder()
+        emailField.resignFirstResponder()
+        passwordField.resignFirstResponder()
         
+        NotificationCenter.default.removeObserver(self)
+
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
@@ -38,20 +77,30 @@ class SignUpViewController: UIViewController {
     }
     
     @IBAction func goBack(_ sender: UIButton) {
+        self.dismiss(animated: false, completion: nil)
     }
     
-    @objc func keyboaardWillAppear(notification: NSNotification) {
+    @objc func keyboardWillAppear(notification: NSNotification) {
+        let info = notification.userInfo!
+        let keyboardFrame: CGRect = (info[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
         
+        continueButton.center = CGPoint(x: view.center.x,
+                                        y: view.frame.height - keyboardFrame.height - 16.0 - continueButton.frame.height / 2)
+        activityView.center = continueButton.center
     }
     
     @objc func textFieldChanged(_ target: UITextField) {
-        
+        let username = userNameField.text
+        let email = emailField.text
+        let password = passwordField.text
+        let formFilled = username != nil && username != "" && email != nil && email != "" && password != nil && password != ""
+        setContinueButton(enabled: formFilled)
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         switch textField {
-        case usernameField:
-            usernameField.resignFirstResponder()
+        case userNameField:
+            userNameField.resignFirstResponder()
             emailField.becomeFirstResponder()
             break
         case emailField:
@@ -67,11 +116,33 @@ class SignUpViewController: UIViewController {
         return true
     }
     
-    func setContinueButton(enable: Bool) {
-        
+    func setContinueButton(enabled: Bool) {
+        if enabled {
+            continueButton.alpha = 1.0
+            continueButton.isEnabled = true
+        } else {
+            continueButton.alpha = 0.5
+            continueButton.isEnabled = false
+        }
     }
     
     @objc func handleSignUp() {
+        guard let username = userNameField.text else { return }
+        guard let email = emailField.text else { return }
+        guard let pass = passwordField.text else { return }
+        
+        setContinueButton(enabled: false)
+        continueButton.setTitle("", for: .normal)
+        activityView.startAnimating()
+        
+        Auth.auth().createUser(withEmail: email, password: pass) { (user, error) in
+            if error == nil && user != nil {
+                print("User created!")
+            } else {
+                print("Error creating user: \(error!.localizedDescription)")
+            }
+        }
+        
         
     }
     
