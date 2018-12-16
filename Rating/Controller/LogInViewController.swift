84b966108656dd682cf5c3550c50f30c3d22cd39
ZIYOCHIRO <ziyochiro@gmail.com>
2018-12-16 08:18:45 +0800

    Set login VC and signUp VC

--- Rating/Controller/LogInViewController.swift
+++ Rating/Controller/LogInViewController.swift
@@ -9,6 +9,118 @@
 import Foundation
 import UIKit
 
-class LogInViewController: UIViewController {
+class LogInViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var goBackButton: UIButtonX!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    var continueButton: RoundedWhiteButton!
+    var activityView: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addVerticalGradientLayer(topColor: primaryColor, bottomColor: secondaryColor)
+        
+        continueButton = RoundedWhiteButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+        continueButton.setTitleColor(secondaryColor, for: .normal)
+        continueButton.setTitle("Continue", for: .normal)
+        continueButton.titleLabel?.font = UIFont.systemFont(ofSize: 18.0, weight: UIFont.Weight.bold)
+        continueButton.center = CGPoint(x: view.center.x, y: view.frame.height - continueButton.frame.height - 24)
+        continueButton.highlightedColor = UIColor(white: 1.0, alpha: 1.0)
+        continueButton.defaultColor = UIColor.white
+        continueButton.addTarget(self, action: #selector(handleSignIn), for: .touchUpInside)
+        continueButton.alpha = 0.5
+        view.addSubview(continueButton)
+        setContinueButton(enabled: false)
+        
+        activityView = UIActivityIndicatorView(style: .gray)
+        activityView.color = secondaryColor
+        activityView.frame = CGRect(x: 0, y: 0, width: 50.0, height: 50.0)
+        activityView.center = continueButton.center
+        
+        view.addSubview(activityView)
+        
+        emailField.delegate = self
+        passwordField.delegate = self
+        
+        emailField.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
+        passwordField.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        emailField.becomeFirstResponder()
+        NotificationCenter.default.addObserver(self, selector:#selector(keyboardWillAppear), name: UIResponder.keyboardWillShowNotification, object: nil)
+
+        
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        emailField.resignFirstResponder()
+        passwordField.resignFirstResponder()
+        NotificationCenter.default.removeObserver(self)
+
+    }
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        get {
+            return .lightContent
+        }
+    }
+    @IBAction func goBack(_ sender: Any) {
+        self.dismiss(animated: false, completion: nil)
+    }
+    @objc func keyboardWillAppear(notification: NSNotification) {
+        let info = notification.userInfo!
+        let keyboardFrame: CGRect = (info[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        
+        continueButton.center = CGPoint(x: view.center.x,
+                                        y: view.frame.height - keyboardFrame.height - 16.0 - continueButton.frame.height / 2)
+        activityView.center = continueButton.center
+    }
+    
+    @objc func textFieldChanged(_ target: UITextField) {
+        let email = emailField.text
+        let password = passwordField.text
+        let formFilled = email != nil && email != "" && password != nil && password != ""
+        setContinueButton(enabled: formFilled)
+    }
+    
+    @objc func handleSignIn() {
+        guard let email = emailField.text else { return }
+        guard let pass = passwordField.text else { return }
+        
+        setContinueButton(enabled: false)
+        continueButton.setTitle("", for: .normal)
+        activityView.startAnimating()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switch textField {
+        case emailField:
+            emailField.resignFirstResponder()
+            passwordField.becomeFirstResponder()
+            break
+        case passwordField:
+            handleSignIn()
+            break
+        default:
+            break
+        }
+        return true
+    }
+    
+    func setContinueButton(enabled:Bool) {
+        if enabled {
+            continueButton.alpha = 1.0
+            continueButton.isEnabled = true
+        } else {
+            continueButton.alpha = 0.5
+            continueButton.isEnabled = false
+        }
+    }
     
 }
--- Rating/Controller/SignUpViewContoller.swift
+++ Rating/Controller/SignUpViewContoller.swift
@@ -10,5 +10,69 @@ import Foundation
 import UIKit
 
 class SignUpViewController: UIViewController {
+    @IBOutlet weak var goBackButton: UIButtonX!
+    @IBOutlet weak var userNameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    var continueButton: RoundedWhiteButton!
+    var activityView: UIActivityIndicatorView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        get {
+            return .lightContent
+        }
+    }
+    
+    @IBAction func goBack(_ sender: UIButton) {
+    }
+    
+    @objc func keyboaardWillAppear(notification: NSNotification) {
+        
+    }
+    
+    @objc func textFieldChanged(_ target: UITextField) {
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switch textField {
+        case usernameField:
+            usernameField.resignFirstResponder()
+            emailField.becomeFirstResponder()
+            break
+        case emailField:
+            emailField.resignFirstResponder()
+            passwordField.becomeFirstResponder()
+            break
+        case passwordField:
+            handleSignUp()
+            break
+        default:
+            break
+        }
+        return true
+    }
+    
+    func setContinueButton(enable: Bool) {
+        
+    }
+    
+    @objc func handleSignUp() {
+        
+    }
     
 }
